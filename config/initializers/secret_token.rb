e3891798b1c27567cf24cfcb5e02b9cb96a5f99a
Dorian Karter <dkarter@gmail.com>
2013-10-20 16:23:00 -0500

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaskMate::Application.config.secret_key_base = '58fdea943d8c38d94e9bb40391993d26a1fecebedf5835c97d5c4b0d77942a7cb23d4fae3a45f424618f87ec3107beffc95cc3fe18b8454b49f82fa47b9cab4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaskMate::Application.config.session_store :cookie_store, key: '_TaskMate_session'
